a8eabde37ebbb343c94528ee8c4b72694c0a47e0
Jon Huntley <jon.huntley@hotmail.co.uk>
2020-01-13 22:54:13 +0000

    Added HTTP Func with DI

---
+++ FuncAppPocHttp/Services/FuncParagraphReader.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using FuncAppPoc.Domain.Model;
+using FuncAppPoc.ParagraphProcessor.Services;
+
+namespace FuncAppPocHttp
+{
+    public class FuncParagraphReader
+    {
+        private readonly IParagraphAnalyser _analyser;
+
+        public FuncParagraphReader(IParagraphAnalyser analyser)
+        {
+            _analyser = analyser;
+        }
+
+        [FunctionName("FuncParagraphReader")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");            
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var message = JsonConvert.DeserializeObject<ParaMessage>(requestBody, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            var response = string.Empty;
+
+            if (message != null)
+            {
+                var result = _analyser.Analyse(message.Sample);
+
+                response = $"Max word count is {result.MaxWordCount}";
+            }
+
+            return (ActionResult)new OkObjectResult($"Hello, {response}");
+        }
+    }
+}
---
+++ FuncAppPocHttp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using FuncAppPoc.ParagraphProcessor.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FuncAppPocHttp.Startup))]
+namespace FuncAppPocHttp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.AddHttpClient();
+
+            //builder.Services.AddSingleton((s) => {
+            //    return new MyService();
+            //});
+
+            //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+
+            builder.Services.AddSingleton<IParagraphAnalyser, ParagraphAnalyser>();
+        }
+    }
+}
